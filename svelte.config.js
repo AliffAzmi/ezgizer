40bfa06c6df0bbfd705d408e1f7b5c8bbac6b47c
Aliff Azmi <aliff.azmi98@gmail.com>
2022-12-29 16:46:23 +0800

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 // import adapter from '@sveltejs/adapter-auto'
-// import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter: adapter()
+		adapter: adapter()
 	},
 	preprocess: [
 		preprocess({
