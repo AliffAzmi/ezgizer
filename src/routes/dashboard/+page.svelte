5e50a8bf791d3c87a0cbee63e2b4cabe5a656923
Aliff Azmi <aliff.azmi98@gmail.com>
2023-06-14 18:03:05 +0800

    update

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -187,7 +187,7 @@
 								class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 								type="button"
 							>
-								<a href="/utilities">See all</a>
+								<a href="/utilities?period={periodKey}">See all</a>
 							</button>
 						</div>
 					</div>
