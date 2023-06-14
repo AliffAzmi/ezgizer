317e472edab5225165dc80b88c5c58f5f0b02c6e
Aliff Azmi <aliff.azmi98@gmail.com>
2023-06-14 15:14:09 +0800

    update link

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -185,8 +185,10 @@
 						<div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
 							<button
 								class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-								type="button">See all</button
+								type="button"
 							>
+								<a href="/utilities">See all</a>
+							</button>
 						</div>
 					</div>
 				</div>
@@ -256,7 +258,7 @@
 						<div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
 							<button
 								class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-								type="button">See all</button
+								type="button"><a href="/categories">See all</a></button
 							>
 						</div>
 					</div>
